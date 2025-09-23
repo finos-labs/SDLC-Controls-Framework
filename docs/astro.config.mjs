1f0d412f79d9cd6cb6cb9cd5e4e97e880dd93041
Toby <toby.weston@gmail.com>
2025-09-23 07:53:22 +0100

    ...just rolls off the tongue

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -6,8 +6,8 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			title: 'CSDLCC',
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/finos-labs/SDLC-Controls-Framework' }],
 			sidebar: [
 				{
 					label: 'Guides',
